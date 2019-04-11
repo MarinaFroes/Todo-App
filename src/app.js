3b76a3895b18e53346a60f57b87ef834959191b2
MFAC <ninafroes@gmail.com>
2019-04-11 15:35:49 +0200

    feat: Add task only if input is truthy

--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   let todoContent = document.querySelector('input[type="text"]').value;
-
-  todoList.insertAdjacentHTML('beforeend', `<li class="todo"><span class="todo-text">${todoContent}</span><button class="far fa-trash-alt delete"></button></li>`);
+  
+  if (todoContent) {
+    todoList.insertAdjacentHTML('beforeend', `<li class="todo"><span class="todo-text">${todoContent}</span><button class="far fa-trash-alt delete"></button></li>`);
+    document.querySelector('input[type="text"]').innerContent = '';
+  }
+  
 })
 
 todoList.addEventListener('click', function (e) {
-  console.log(e.target.nodeName);
   if (e.target.nodeName === 'BUTTON') {
     e.target.parentElement.remove();
   } else if (e.target.nodeName === 'SPAN' || e.target.nodeName === 'LI') {
     e.target.classList.toggle('done');
   }
 })
+
+
