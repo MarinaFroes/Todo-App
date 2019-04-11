2400fe42435477c9e640631152569f966c86e4fb
MFAC <ninafroes@gmail.com>
2019-04-11 14:08:32 +0200

    feat: Add tasks to list

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const form = document.getElementsByTagName('form')[0];
+const todoList = document.querySelector('.todo-container');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let todoContent = document.querySelector('input[type="text"]').value;
+
+  todoList.insertAdjacentHTML('beforeend', `<li class="todo"><span class="todo-text">${todoContent}</span><button class="delete">X</button></li>`);
+})
+
+
+
